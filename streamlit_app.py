7bf115991ae3666e6498fe4a4685d43934b85abb
melliddytraining <128511992+melliddytraining@users.noreply.github.com>
2023-03-21 18:14:43 +0000

    Update streamlit_app.py
    filter list

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Add picker
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 #Display file via a data frame on the page
 streamlit.dataframe(my_fruit_list)
 
