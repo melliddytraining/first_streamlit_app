06095909ca10c435718340b521ba183abb059f7a
melliddytraining <128511992+melliddytraining@users.noreply.github.com>
2023-03-21 18:17:46 +0000

    Update streamlit_app.py
    Add variable for selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Add picker
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+
 #Display file via a data frame on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
