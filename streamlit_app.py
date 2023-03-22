bcb43518bf5fa326c773f1a50f3d7731913d0a18
melliddytraining <128511992+melliddytraining@users.noreply.github.com>
2023-03-22 12:15:25 +0000

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/+"Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"Kiwi")
 
 #take the json version of response and normalise it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
