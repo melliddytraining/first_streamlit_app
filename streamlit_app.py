3bfa1a11b986e1dc5fbd750b7c5f3be08dfbffa9
melliddytraining <128511992+melliddytraining@users.noreply.github.com>
2023-03-24 15:53:46 +0000

    Update streamlit_app.py
    Indent incorrect

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ try:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
   
- except URLError as e:
-    streamlit.error()
+except URLError as e:
+  streamlit.error()
   
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
